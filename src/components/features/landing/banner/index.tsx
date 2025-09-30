edf3cef516d1bbf09e0a3cd79bfae7cd0b562cc3
sadamhusein77.sh <sadamhusein77.sh@gmail.com>
2025-09-30 20:02:14 +0700

    feat: [CODE-0000] [SADAM] adj banner landing desktop screen

--- src/components/features/landing/banner/index.tsx
+++ src/components/features/landing/banner/index.tsx
@@ -1,13 +1,19 @@
 import { imgSadamPhoto, imgWave } from '@/assets';
-import style from './style.module.scss';
 import Chip from '@/components/ui/chip';
 import Button from '@/components/ui/button';
 import { ArrowRightIcon } from '@/assets/svgs/ico_chevron-right';
 import { useNavigate } from 'react-router';
+import { motion } from 'framer-motion';
+import style from './style.module.scss';
 
 const ProfilePhoto = () => {
   return (
-    <div className={style.banner__photo}>
+    <motion.div
+      className={style.banner__photo}
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8 }}
+    >
       <div className={style.banner__photo__wrapper}>
         <img
           src={imgSadamPhoto}
@@ -16,32 +22,48 @@ const ProfilePhoto = () => {
         />
       </div>
       <img src={imgWave} alt="img-wave" className={style.banner__photo__bg} />
-    </div>
+    </motion.div>
   );
 };
 
 const GreetingCard = () => {
   const navigate = useNavigate();
   return (
-    <div className={style.banner__greeting}>
+    <motion.div
+      className={style.banner__greeting}
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1 }}
+    >
       <h4 className={style.banner__greeting__title}>Greetings!</h4>
-      <p className={style.banner__greeting__desc}>I'm a dedicated Front-End Developer with over 4 years of hands-on experience crafting dynamic and responsive user interfaces. My expertise lies in the <span className='text-black'>React.js</span> ecosystem, where I specialize in building <span className='text-black'>efficient</span>, <span className='text-black'>scalable</span>, and visually appealing web applications. I am passionate about creating seamless user experiences and translating complex designs into <span className='text-black'>clean, maintainable code</span>.</p>
-      <Button className={style.banner__greeting__cta} onClick={() => navigate('/project')}>
+      <p className={style.banner__greeting__desc}>
+        I'm a dedicated Front-End Developer with over 4 years of hands-on
+        experience crafting dynamic and responsive user interfaces. My expertise
+        lies in the <span className="text-black">React.js</span> ecosystem,
+        where I specialize in building{' '}
+        <span className="text-black">efficient</span>,{' '}
+        <span className="text-black">scalable</span>, and visually appealing web
+        applications. I am passionate about creating seamless user experiences
+        and translating complex designs into{' '}
+        <span className="text-black">clean, maintainable code</span>.
+      </p>
+      <Button
+        className={style.banner__greeting__cta}
+        onClick={() => navigate('/project')}
+      >
         <span>Explore Projects</span>
-        <ArrowRightIcon color="white" />  
+        <ArrowRightIcon color="white" />
       </Button>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default function Banner() {
   return (
     <section className={style.banner}>
       <ProfilePhoto />
       <div className={style.banner__info}>
-        <Chip className={style.banner__info__name}>
-          Sadam Husein
-        </Chip>
+        <Chip className={style.banner__info__name}>Sadam Husein</Chip>
         <span className={style.banner__info__job}>Front-end Web Developer</span>
       </div>
       <GreetingCard />
